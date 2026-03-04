3ccf81636c40179a82d748beebe383cb6895c74e
mm56-3 <142387764+mm56-3@users.noreply.github.com>
2026-03-04 14:02:59 +0530

    Add quiz management functions in admin.js
    Implement functions to add questions, reset leaderboard, and load players from local storage.

---
+++ admin.js
@@ -0,0 +1,39 @@
+function addQuestion(){
+
+let questions =
+JSON.parse(localStorage.getItem("quizQuestions")) || [];
+
+questions.push({
+question:q.value,
+options:[o1.value,o2.value,o3.value,o4.value],
+answer:ans.value
+});
+
+localStorage.setItem(
+"quizQuestions",
+JSON.stringify(questions)
+);
+
+alert("Question Added!");
+}
+
+function resetBoard(){
+localStorage.removeItem("leaderboard");
+alert("Leaderboard Reset");
+}
+
+function loadPlayers(){
+
+let leaderboard =
+JSON.parse(localStorage.getItem("leaderboard")) || [];
+
+let list=document.getElementById("players");
+
+leaderboard.forEach(p=>{
+let li=document.createElement("li");
+li.textContent=p.name+" - "+p.score;
+list.appendChild(li);
+});
+}
+
+loadPlayers();
