d422e496b70415b01facd7969acca61fb202fe8d
mm56-3 <142387764+mm56-3@users.noreply.github.com>
2026-03-07 01:35:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,249 +1,67 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
-
+import { db } from "./firebase.js";
 import {
-getFirestore,
 collection,
-addDoc,
-getDocs
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js"
-
-import {
-getStorage,
-ref,
-uploadBytes,
-getDownloadURL
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js"
-
-
-const firebaseConfig = {
-
-apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
-authDomain: "prahelika-quiz-forum.firebaseapp.com",
-projectId: "prahelika-quiz-forum",
-storageBucket: "prahelika-quiz-forum.firebasestorage.app",
-messagingSenderId: "593115491592",
-appId: "1:593115491592:web:242cb215013a3bdab850bd"
-
-}
-
-const app = initializeApp(firebaseConfig)
-
-const db = getFirestore(app)
-
-const storage = getStorage(app)
-
-
-
-window.show = function(id){
-
-document.querySelectorAll("section").forEach(s=>s.classList.remove("active"))
-
-document.getElementById(id).classList.add("active")
-
-}
-
-
+getDocs,
+addDoc
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 let questions=[]
 let index=0
 let score=0
-let timer
-
 
 window.startQuiz=async function(){
 
-document.getElementById("quizArea").style.display="block"
+const snap=await getDocs(collection(db,"questions"))
 
-const qSnap=await getDocs(collection(db,"questions"))
-
-questions=qSnap.docs.map(d=>d.data())
+questions=snap.docs.map(d=>d.data())
 
 index=0
+score=0
 
 showQuestion()
 
 }
 
-
-
 function showQuestion(){
 
-let q=questions[index]
+const q=questions[index]
 
 document.getElementById("question").innerText=q.question
 
-let opt=""
+let html=""
 
 q.options.forEach(o=>{
-
-opt+=`<button onclick="check('${o}')">${o}</button>`
-
+html+=`<button onclick="answer('${o}')">${o}</button>`
 })
 
-document.getElementById("options").innerHTML=opt
+document.getElementById("options").innerHTML=html
 
 startTimer()
 
 }
 
+window.answer=function(a){
 
-
-window.check=function(o){
-
-if(o===questions[index].answer)score++
-
+if(a===questions[index].answer){
+score++
 }
 
-
+}
 
 window.nextQuestion=function(){
 
 index++
 
 if(index<questions.length){
-
 showQuestion()
-
-}
-
-else{
-
-saveScore()
-
+}else{
+finishQuiz()
 }
 
 }
 
+function finishQuiz(){
 
-
-function startTimer(){
-
-let t=15
-
-document.getElementById("timer").innerText=t
-
-timer=setInterval(()=>{
-
-t--
-
-document.getElementById("timer").innerText=t
-
-if(t<=0){
-
-clearInterval(timer)
-
-nextQuestion()
-
-}
-
-},1000)
-
-}
-
-
-
-async function saveScore(){
-
-await addDoc(collection(db,"scores"),{
-
-name:document.getElementById("playerName").value,
-
-phone:document.getElementById("playerPhone").value,
-
-score:score
-
-})
-
-alert("Quiz Finished")
-
-}
-
-
-
-window.adminLogin=function(){
-
-let u=document.getElementById("adminUser").value
-
-let p=document.getElementById("adminPass").value
-
-if(u==="VIKRAM784125" && p==="#UDSB784125781005"){
-
-document.getElementById("adminPanel").style.display="block"
-
-}
-
-}
-
-
-
-window.addQuestion=async function(){
-
-await addDoc(collection(db,"questions"),{
-
-question:q.value,
-
-options:[o1.value,o2.value,o3.value,o4.value],
-
-answer:ans.value
-
-})
-
-alert("Question Added")
-
-}
-
-
-
-window.uploadGallery=async function(){
-
-let file=galleryUpload.files[0]
-
-let r=ref(storage,"gallery/"+file.name)
-
-await uploadBytes(r,file)
-
-alert("Uploaded")
-
-}
-
-
-
-window.uploadAchievement=async function(){
-
-let file=achieveUpload.files[0]
-
-let r=ref(storage,"achievements/"+file.name)
-
-await uploadBytes(r,file)
-
-alert("Uploaded")
-
-}
-
-
-
-window.uploadLogo=async function(){
-
-let file=logoUpload.files[0]
-
-let r=ref(storage,"logo/"+file.name)
-
-await uploadBytes(r,file)
-
-alert("Logo Updated")
-
-}
-
-
-
-window.addExecutive=async function(){
-
-let file=exPhoto.files[0]
-
-let r=ref(storage,"executives/"+file.name)
-
-await uploadBytes(r,file)
-
-alert("Executive Added")
+alert("Quiz finished. Score: "+score)
 
 }
