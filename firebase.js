d9ac3e718fcf74ef07fa2474b9eebdb2d7fa1a82
mm56-3 <142387764+mm56-3@users.noreply.github.com>
2026-03-07 01:33:38 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
-// Firebase configuration
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
-  authDomain: "prahelika-quiz-forum.firebaseapp.com",
-  projectId: "prahelika-quiz-forum",
-  storageBucket: "prahelika-quiz-forum.firebasestorage.app",
-  messagingSenderId: "593115491592",
-  appId: "1:593115491592:web:242cb215013a3bdab850bd"
+apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
+authDomain: "prahelika-quiz-forum.firebaseapp.com",
+projectId: "prahelika-quiz-forum",
+storageBucket: "prahelika-quiz-forum.appspot.com",
+messagingSenderId: "593115491592",
+appId: "1:593115491592:web:242cb215013a3bdab850bd"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
-// Database
-const db = firebase.firestore();
-
-// Authentication
-const auth = firebase.auth();
+export const db = getFirestore(app);
+export const storage = getStorage(app);
