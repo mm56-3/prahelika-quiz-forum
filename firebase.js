7245cd92b041a5f8e3bc107e9cc0d691278c1fe0
mm56-3 <142387764+mm56-3@users.noreply.github.com>
2026-03-07 01:58:01 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
-authDomain: "prahelika-quiz-forum.firebaseapp.com",
-projectId: "prahelika-quiz-forum",
-storageBucket: "prahelika-quiz-forum.appspot.com",
-messagingSenderId: "593115491592",
-appId: "1:593115491592:web:242cb215013a3bdab850bd"
+  apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
+  authDomain: "prahelika-quiz-forum.firebaseapp.com",
+  projectId: "prahelika-quiz-forum",
+  storageBucket: "prahelika-quiz-forum.firebasestorage.app",
+  messagingSenderId: "593115491592",
+  appId: "1:593115491592:web:242cb215013a3bdab850bd"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const db = getFirestore(app);
-export const storage = getStorage(app);
