79bdba414e47acb994cda62c0cbe928ecd0e2acb
mm56-3 <142387764+mm56-3@users.noreply.github.com>
2026-03-07 03:18:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,17 +1,19 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
-  authDomain: "prahelika-quiz-forum.firebaseapp.com",
-  projectId: "prahelika-quiz-forum",
-  storageBucket: "prahelika-quiz-forum.firebasestorage.app",
-  messagingSenderId: "593115491592",
-  appId: "1:593115491592:web:242cb215013a3bdab850bd"
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCy2BYWDIcbczywlT2UAV7BX8XAqChah88",
+    authDomain: "prahelika-quiz-forum.firebaseapp.com",
+    projectId: "prahelika-quiz-forum",
+    storageBucket: "prahelika-quiz-forum.firebasestorage.app",
+    messagingSenderId: "593115491592",
+    appId: "1:593115491592:web:450ebaec556c04edb850bd"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
